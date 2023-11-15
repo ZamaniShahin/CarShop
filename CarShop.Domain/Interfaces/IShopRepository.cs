7dfd527018fe5b780167be18b61212542792ff31
ZamaniShahin <sh.zamani2002@gmail.com>
2023-11-15 21:26:39 +0330

    IShopRepository Implemented

--- CarShop.Domain/Interfaces/IShopRepository.cs
+++ CarShop.Domain/Interfaces/IShopRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace CarShop.Domain.Interfaces
+﻿using CarShop.Domain.Models;
+
+namespace CarShop.Domain.Interfaces
 {
     public interface IShopRepository
     {
-        
+        public bool CreateCategory(CreateCarCategory command);
+        public bool EditCarCategory(EditCarCategory command);
+        public bool DeleteCarCategory(long id);
+        public CarCategory GetCategory(long id);
+        public List<CarCategory> GetAllCategories();
     }
 }
