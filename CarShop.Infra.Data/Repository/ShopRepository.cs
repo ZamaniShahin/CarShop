903896cba5fc24b905fe706fbd53648d85223e6b
ZamaniShahin <sh.zamani2002@gmail.com>
2023-11-15 21:49:47 +0330

    ShopRepository.cs created and Inheritted

---
+++ CarShop.Infra.Data/Repository/ShopRepository.cs
@@ -0,0 +1,32 @@
+﻿using CarShop.Domain.Interfaces;
+using CarShop.Domain.Models;
+
+namespace CarShop.Infra.Data.Repository;
+
+public class ShopRepository:IShopRepository
+{
+    public bool CreateCategory(CreateCarCategory command)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool EditCarCategory(EditCarCategory command)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeleteCarCategory(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public CarCategory GetCategory(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<CarCategory> GetAllCategories()
+    {
+        throw new NotImplementedException();
+    }
+}
