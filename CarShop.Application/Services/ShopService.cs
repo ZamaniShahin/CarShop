ebae6ea218025c2df6723838fe1a8389ec153768
ZamaniShahin <sh.zamani2002@gmail.com>
2023-11-15 21:34:21 +0330

    ShopService.cs Inherited

--- CarShop.Application/Services/ShopService.cs
+++ CarShop.Application/Services/ShopService.cs
@@ -1,6 +1,39 @@
-﻿namespace CarShop.Application.Services;
+﻿using CarShop.Application.Interfaces;
+using CarShop.Domain.Interfaces;
+using CarShop.Domain.Models;
 
-public class ShopService
+namespace CarShop.Application.Services;
+
+public class ShopService:IShopService
 {
-    
+    private readonly IShopRepository _shopRepository;
+
+    public ShopService(IShopRepository shopRepository)
+    {
+        _shopRepository = shopRepository;
+    }
+    public bool CreateCategory(CreateCarCategory command)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool EditCarCategory(EditCarCategory command)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeleteCarCategory(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public CarCategory GetCategory(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<CarCategory> GetAllCategories()
+    {
+        throw new NotImplementedException();
+    }
 }
