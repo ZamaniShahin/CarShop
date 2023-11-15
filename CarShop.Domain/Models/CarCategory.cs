05947960621b902e0278577fa8c343b8f79935a7
ZamaniShahin <sh.zamani2002@gmail.com>
2023-11-15 21:17:09 +0330

    CarCategory.cs Implemented

---
+++ CarShop.Domain/Models/CarCategory.cs
@@ -0,0 +1,15 @@
+﻿namespace CarShop.Domain.Models
+{
+    public class CarCategory(string categoryName)
+    {
+        public long Id { get; set; }
+        public string CategoryName { get; set; } = categoryName;
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        public void Edit(string categoryName, DateTime dateCreated)
+        {
+            CategoryName = categoryName;
+            DateCreated = DateTime.Now;
+        }
+    }
+}
