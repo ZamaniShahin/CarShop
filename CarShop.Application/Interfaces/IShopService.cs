58cbaa47e691e72b5686b389ec4769f5427dab1f
ZamaniShahin <sh.zamani2002@gmail.com>
2023-11-15 21:32:46 +0330

    IShopService Implemented

---
+++ CarShop.Application/Interfaces/IShopService.cs
@@ -0,0 +1,12 @@
+﻿using CarShop.Domain.Models;
+
+namespace CarShop.Application.Interfaces;
+
+public interface IShopService
+{
+    public bool CreateCategory(CreateCarCategory command);
+    public bool EditCarCategory(EditCarCategory command);
+    public bool DeleteCarCategory(long id);
+    public CarCategory GetCategory(long id);
+    public List<CarCategory> GetAllCategories();
+}
