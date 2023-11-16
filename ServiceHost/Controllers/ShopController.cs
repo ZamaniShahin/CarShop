bdb3c733695d79122cec444078275a94111622f5
ZamaniShahin <sh.zamani2002@gmail.com>
2023-11-16 19:38:30 +0330

    ShopController Implemented

---
+++ ServiceHost/Controllers/ShopController.cs
@@ -0,0 +1,25 @@
+using CarShop.Domain.Interfaces;
+using CarShop.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShopController : ControllerBase
+    {
+        private readonly IShopRepository _shopRepository;
+
+        public ShopController(IShopRepository shopRepository)
+        {
+            _shopRepository = shopRepository;
+        }
+
+        [HttpGet(Name = "GetIndex")]
+        public List<CarCategory> Get()
+        {
+            var list = _shopRepository.GetAllCategories().ToList();
+            return list;
+        }
+    }
+}
