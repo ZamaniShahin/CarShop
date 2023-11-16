9fedcc8712be5a2bdfd45b010f178bc50356cc49
ZamaniShahin <sh.zamani2002@gmail.com>
2023-11-16 23:50:04 +0330

    Cars Table Configured in ShopContext

--- CarShop.Infra.Data/Context/ShopContext.cs
+++ CarShop.Infra.Data/Context/ShopContext.cs
@@ -6,4 +6,5 @@ namespace CarShop.Infra.Data.Context;
 public class ShopContext(DbContextOptions<ShopContext> options) : DbContext(options)
 {
     public DbSet<CarCategory> CarCategories { get; set; }
+    public DbSet<Car> Cars { get; set; }
 }
