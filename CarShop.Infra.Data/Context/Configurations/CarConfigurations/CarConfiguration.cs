d1465a999e1adb6a39a144c1601065a11f56363d
ZamaniShahin <sh.zamani2002@gmail.com>
2023-11-17 13:03:00 +0330

    CarConfiguration.cs Implemented

---
+++ CarShop.Infra.Data/Context/Configurations/CarConfigurations/CarConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using CarShop.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarShop.Infra.Data.Context.Configurations.CarConfigurations
+{
+    public class CarConfiguration:IEntityTypeConfiguration<Car>
+    {
+        public void Configure(EntityTypeBuilder<Car> builder)
+        {
+            //builder.ToTable("Cars");
+            builder.HasKey(x => x.CarId);
+            builder.Property(x => x.CarBrand).HasMaxLength(250).IsRequired();
+            builder.Property(x => x.CarName).HasMaxLength(250).IsRequired();
+            builder.Property(x => x.CarColor).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.CarCreatedYear).HasMaxLength(250).IsRequired();
+            builder.Property(x => x.DateCarCreatedBanner).HasMaxLength(250).IsRequired();
+        }
+    }
+}
